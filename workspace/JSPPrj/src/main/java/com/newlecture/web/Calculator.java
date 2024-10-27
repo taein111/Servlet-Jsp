907bfbf16da1861a3b85b13835e9b2614b3e7bf9
taein111 <taein4546@naver.com>
2024-10-27 12:18:34 +0900

    Jsp work

--- workspace/JSPPrj/src/main/java/com/newlecture/web/Calculator.java
+++ workspace/JSPPrj/src/main/java/com/newlecture/web/Calculator.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/SScalculator")
+@WebServlet("/calculator")
 public class Calculator extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
